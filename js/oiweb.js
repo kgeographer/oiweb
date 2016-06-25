66bd06cf2aa173469c8199d604f81d4e8c9ff796
kgeographer <karlg@kgeographer.org>
2016-06-25 13:16:38 -0700

    initial commit

---
+++ js/oiweb.js
@@ -0,0 +1,5 @@
+
+
+L.mapbox.accessToken = 'pk.eyJ1Ijoia2dlb2dyYXBoZXIiLCJhIjoiUmVralBPcyJ9.mJegAI1R6KR21x_CVVTlqw';
+var map = L.mapbox.map('map', 'mapbox.mapbox-terrain-v2')
+    .setView([20.1651, 100.3947], 6);
